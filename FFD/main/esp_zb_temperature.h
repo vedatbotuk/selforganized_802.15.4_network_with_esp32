290f19a1bcd2e7caff50fb334bd159a043f9e576
Vedat <vedat.botuk@gmail.com>
2023-10-16 14:44:25 +0200

    FFD Device configured

--- FFD/main/esp_zb_temperature.h
+++ FFD/main/esp_zb_temperature.h
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: CC0-1.0
  *
- * Zigbee customized server Example
+ * Zigbee HA_TEMPERATURE
  *
  * This example code is in the Public Domain (or CC0 licensed, at your option.)
  *
@@ -16,19 +16,20 @@
 #include "light_driver.h"
 
 /* Zigbee configuration */
-#define MAX_CHILDREN                    10          /* the max amount of connected devices */
-#define INSTALLCODE_POLICY_ENABLE       false    /* enable the install code policy for security */
-#define HA_ESP_LIGHT_ENDPOINT           10          /* esp light bulb device endpoint */
-#define ESP_ZB_PRIMARY_CHANNEL_MASK     (1l << 13)  /* Zigbee primary channel mask use in the example */
+#define INSTALLCODE_POLICY_ENABLE       false  /* enable the install code policy for security */
+#define MAX_CHILDREN                    10     /* the max amount of connected devices */
+#define HA_ESP_TEMPERATURE_ENDPOINT     10     /* esp light bulb device endpoint, used to process light controlling commands */
+#define ESP_ZB_PRIMARY_CHANNEL_MASK     (1l << 11)  /* Zigbee primary channel mask use in the example */
 
-#define ESP_ZB_ZC_CONFIG()                                                              \
+#define ESP_ZB_ZR_CONFIG()                                                              \
     {                                                                                   \
-        .esp_zb_role = ESP_ZB_DEVICE_TYPE_COORDINATOR,                                  \
+        .esp_zb_role = ESP_ZB_DEVICE_TYPE_ROUTER,                                       \
         .install_code_policy = INSTALLCODE_POLICY_ENABLE,                               \
         .nwk_cfg.zczr_cfg = {                                                           \
             .max_children = MAX_CHILDREN,                                               \
         },                                                                              \
     }
+
 
 #define ESP_ZB_DEFAULT_RADIO_CONFIG()                           \
     {                                                           \
