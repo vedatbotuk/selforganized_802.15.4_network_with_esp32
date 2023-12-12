1325a11345c8fdde797ded6d146fe14c539bb665
Vedat <vedat.botuk@gmail.com>
2023-12-12 23:53:35 +0100

    some changes

--- components/battery/battery_read.c
+++ components/battery/battery_read.c
@@ -11,10 +11,12 @@
 #include "soc/soc_caps.h"
 #include "esp_log.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 const static char *TAG_VOL = "VOLTAGE";
 
-#define VOLTAGE_MAX 8200
+#define VOLTAGE_MAX 9000
 #define VOLTAGE_MIN 7000
 
 /*---------------------------------------------------------------
@@ -30,31 +32,49 @@ const static char *TAG_VOL = "VOLTAGE";
 #define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
 
 adc_oneshot_unit_handle_t adc1_handle;
+adc_cali_handle_t adc1_cali_chan1_handle = NULL;
+bool do_calibration1_chan1;
+
 
 static int adc_raw[2][10];
+static int voltage[2][10];
+//static int battery_percentage;
+//static int battery_percentage;
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 void voltage_calculate_init();
 
 int calc_battery_percentage(int adc)
 {
     int battery_voltage = (float) adc * 519076 / 470000 / 3300 * VOLTAGE_MAX;
+//    ESP_LOGI(TAG_VOL, " ADC Raw: %d", adc);
+//    ESP_LOGI(TAG_VOL, "Battery voltage: %d mV", battery_voltage);
     int battery_percentage = 100 * (battery_voltage - VOLTAGE_MIN) / (VOLTAGE_MAX - VOLTAGE_MIN);
+    // (adc - min measured_value on pin with 8,2V) / (8,2(calculated value) - 7(calculated value))
+//    battery_percentage = 100 * (adc - 3566) / (4178 - 3566);
+//      ESP_LOGI(TAG_VOL, "Battery percentage: %d", battery_percentage);
 
-//    if (battery_percentage < 0)
-//        battery_percentage = 0;
-//    if (battery_percentage > 100)
-//        battery_percentage = 100;
-
+    if (battery_percentage < 0)
+        battery_percentage = 0;
+    if (battery_percentage > 100)
+        battery_percentage = 100;
+    
     return battery_percentage;
 }
 
 int get_battery_level()
 {
     int battery_level = 0;
-
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN1, &adc_raw[0][0]));
-    ESP_LOGI(TAG_VOL, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, adc_raw[0][0]);
-    battery_level = calc_battery_percentage(adc_raw[0][0]);
-
+    
+    if (do_calibration1_chan1) {
+        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan1_handle, adc_raw[0][1], &voltage[0][1]));
+//        ESP_LOGI(TAG_VOL, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, voltage[0][1] );
+        battery_level = calc_battery_percentage(voltage[0][1]);
+    } else {
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN1, &adc_raw[0][0]));
+//        ESP_LOGI(TAG_VOL, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, adc_raw[0][0]);
+        battery_level = calc_battery_percentage(adc_raw[0][0]);
+    }
+    
     return battery_level;
 }
 
@@ -71,4 +91,60 @@ void voltage_calculate_init(){
         .atten = EXAMPLE_ADC_ATTEN,
     };
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN1, &config));
+
+    //-------------ADC1 Calibration Init---------------//
+    do_calibration1_chan1 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN1, EXAMPLE_ADC_ATTEN, &adc1_cali_chan1_handle);
+}
+
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+    adc_cali_handle_t handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG_VOL, "calibration scheme version is %s", "Curve Fitting");
+        adc_cali_curve_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .chan = channel,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG_VOL, "calibration scheme version is %s", "Line Fitting");
+        adc_cali_line_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+    *out_handle = handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG_VOL, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+        ESP_LOGW(TAG_VOL, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(TAG_VOL, "Invalid arg or no memory");
+    }
+
+    return calibrated;
 }
