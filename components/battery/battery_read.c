922589b3fe9587533d7c5e1c35976689ffc50253
vedatbotuk <72791621+vedatbotuk@users.noreply.github.com>
2023-12-14 19:42:04 +0100

    Update battery_read.c

--- components/battery/battery_read.c
+++ components/battery/battery_read.c
@@ -19,30 +19,41 @@ const static char *TAG_VOL = "VOLTAGE";
 #define VOLTAGE_MAX 9000
 #define VOLTAGE_MIN 7000
 
-#define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
-
 /*---------------------------------------------------------------
         ADC General Macros
 ---------------------------------------------------------------*/
 //ADC1 Channels
-#if CONFIG_IDF_TARGET_ESP32C6
-#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_3
-#elif CONFIG_IDF_TARGET_ESP32H2
-#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_0
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_4
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_5
+#else
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_2
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_3
 #endif
+
+#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
+/**
+ * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ */
+#define EXAMPLE_USE_ADC2            1
+#endif
+
+#if EXAMPLE_USE_ADC2
+//ADC2 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#else
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#endif
+#endif  //#if EXAMPLE_USE_ADC2
 
 #define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
 
-adc_oneshot_unit_handle_t adc1_handle;
-adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-bool do_calibration1_chan0;
-
-
 static int adc_raw[2][10];
 static int voltage[2][10];
-//static int battery_percentage;
-//static int battery_percentage;
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 void voltage_calculate_init();
 
 int calc_battery_percentage(int adc)
@@ -78,8 +89,10 @@ esp_err_t get_battery_level(int *battery_level, int *voltage_cal)
     return ESP_OK;
 }
 
-void voltage_calculate_init(){
+void voltage_calculate_init()
+{
     //-------------ADC1 Init---------------//
+    adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
     };
@@ -91,12 +104,31 @@ void voltage_calculate_init(){
         .atten = EXAMPLE_ADC_ATTEN,
     };
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN1, &config));
 
     //-------------ADC1 Calibration Init---------------//
-    adc1_cali_chan0_handle = NULL;
-    do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN0, EXAMPLE_ADC_ATTEN, &adc1_cali_chan0_handle);
-}
+    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    adc_cali_handle_t adc1_cali_chan1_handle = NULL;
+    bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN0, EXAMPLE_ADC_ATTEN, &adc1_cali_chan0_handle);
+    bool do_calibration1_chan1 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN1, EXAMPLE_ADC_ATTEN, &adc1_cali_chan1_handle);
 
+#if EXAMPLE_USE_ADC2
+    //-------------ADC2 Init---------------//
+    adc_oneshot_unit_handle_t adc2_handle;
+    adc_oneshot_unit_init_cfg_t init_config2 = {
+        .unit_id = ADC_UNIT_2,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
+
+    //-------------ADC2 Calibration Init---------------//
+    adc_cali_handle_t adc2_cali_handle = NULL;
+    bool do_calibration2 = example_adc_calibration_init(ADC_UNIT_2, EXAMPLE_ADC2_CHAN0, EXAMPLE_ADC_ATTEN, &adc2_cali_handle);
+
+    //-------------ADC2 Config---------------//
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, EXAMPLE_ADC2_CHAN0, &config));
+#endif  //#if EXAMPLE_USE_ADC2
+}
 
 /*---------------------------------------------------------------
         ADC Calibration
@@ -109,7 +141,7 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG_VOL, "calibration scheme version is %s", "Curve Fitting");
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
         adc_cali_curve_fitting_config_t cali_config = {
             .unit_id = unit,
             .chan = channel,
@@ -125,7 +157,7 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 
 #if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG_VOL, "calibration scheme version is %s", "Line Fitting");
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
         adc_cali_line_fitting_config_t cali_config = {
             .unit_id = unit,
             .atten = atten,
@@ -140,12 +172,24 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 
     *out_handle = handle;
     if (ret == ESP_OK) {
-        ESP_LOGI(TAG_VOL, "Calibration Success");
+        ESP_LOGI(TAG, "Calibration Success");
     } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG_VOL, "eFuse not burnt, skip software calibration");
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
     } else {
-        ESP_LOGE(TAG_VOL, "Invalid arg or no memory");
+        ESP_LOGE(TAG, "Invalid arg or no memory");
     }
 
     return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
+
+#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
+#endif
 }
